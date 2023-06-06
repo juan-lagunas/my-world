b50decb7c8ce36be454db59466d27613256e6f04
Juan Lagunas <juanlagunas28@gmail.com>
2023-06-06 18:16:28 -0400

    change hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function World() {
     return (
         <>
-            <section className="h-[600px] sm:min-h-[1024px] bg-[#1E1725] w-full">
+            <section className="h-[600px] sm:min-h-[1024px] bg-[#0D0B12] w-full">
                 <div className="hero"></div>
             </section>
             <main>
