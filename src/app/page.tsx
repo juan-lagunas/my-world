02fa59721ff92d7b5ed493f412cce4346522eb9e
Juan Lagunas <juanlagunas28@gmail.com>
2023-06-05 21:24:57 -0400

    Add gap to grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 
 function Square({value, onSquareClick}: {value:any, onSquareClick:any}) {
-    return <button className="h-24 sm:w-32 sm:h-32 text-2xl sm:text-3xl font-bold bg-zinc-200 sm:hover:bg-zinc-400 transition ease-in-out" onClick={onSquareClick}>{value}</button>;
+    return <button className="h-24 sm:w-32 sm:h-32 text-2xl sm:text-3xl font-bold bg-zinc-200 sm:hover:bg-zinc-400 transition ease-in-out " onClick={onSquareClick}>{value}</button>;
 }
 
 export default function Board() {
@@ -38,9 +38,9 @@ export default function Board() {
     return (
         <main className="flex min-h-screen w-full">
             <div className="w-full sm:w-auto m-auto text-center text-white">
-                <div className="font-bold text-3xl sm:text-6xl mb-2">Tic-Tac-Toe</div>
-                <div className="font-semibold text-xl mb-2">{status}</div>
-                <div className="w-10/12 sm:w-auto mx-auto grid grid-cols-3 grid-rows-3 text-gray-600 shadow-md rounded-xl overflow-hidden">
+                <div className="font-bold text-3xl sm:text-6xl mb-3">Tic-Tac-Toe</div>
+                <div className="font-semibold text-xl mb-3">{status}</div>
+                <div className="w-10/12 sm:w-auto mx-auto grid grid-cols-3 grid-rows-3 text-gray-600 shadow-md rounded-xl overflow-hidden gap-1">
                     {[...Array(9)].map((e, i) => <Square key={i} value={squares[i]} onSquareClick={() => handleClick(i)}/>)}
                 </div>
             </div>
