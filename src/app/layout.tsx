d5f3a21737f8bd772fdcdbfa16b9a8e6296cecca
Juan Lagunas <juanlagunas28@gmail.com>
2023-06-07 01:34:51 -0400

    Change styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,11 +12,19 @@ export default function RootLayout({
 }) {
     return (
     <html lang="en">
+        <head>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet"/>
+        </head>
         <body className="bg-black">
-            <nav className="fixed h-[52px] w-full z-10 bg-[#161616] bg-opacity-70">
-                <div className="w-3/4 sm:max-w-[980px] h-full mx-auto text-[#ffffff] flex justify-between items-center">
-                    <Link href="/" className="font-bold text-2xl">PBJ</Link>
-                    <Link href="/game" className="font-semibold text-sm">Game</Link>
+            <nav className="h-[52px] w-full font-mono font-extrabold text-2xl">
+                <div className="w-3/4 sm:max-w-[980px] h-full mx-auto text-white flex justify-center items-center gap-3 ">
+                    <Link href="/" className="hover:scale-110 hover:-translate-y-1 transition ease-in-out duration-200">Home</Link>
+                    <span>&#183;</span>
+                    <Link href="/game" className="hover:scale-110 hover:-translate-y-1 transition ease-in-out duration-200">Games</Link>
+                    <span>&#183;</span>
+                    <Link href="/game" className="hover:scale-110 hover:-translate-y-1 transition ease-in-out duration-200">Games</Link>
                 </div>
             </nav>
             {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 export default function World() {
     return (
         <>
-            <section className="h-[600px] sm:min-h-[1024px] bg-[#0D0B12] w-full">
-                <div className="hero"></div>
+            <section className="w-full h-[600px] sm:min-h-[1024px] bg-gradient-to-b from-black to-[#111010] flex">
+                <div className="max-[400px]:h-64 max-[400px]:w-10/12 h-72 w-[400px] sm:w-[600px] sm:h-96 m-auto bg-[#ff1313] rounded-[45px] grid grid-rows-3 shadow-2xl shadow-[rgb(42,56,255)] hover:shadow-[rgb(134,42,255)] hover:scale-110 transiton duration-300 ">
+                    <div className="row-span-1 text-white text-center flex">
+                        <div className="m-auto">
+                            <div className="text-5xl sm:text-7xl font-bold uppercase">Hello</div>
+                            <div className="text-2xl sm:text-3xl font-bold ">my name is</div>
+                        </div>
+                    </div>
+                    <div className="row-span-2 bg-white h-[95%] w-[95%] mx-auto rounded-t-none rounded-b-[45px] flex">
+                        <div className="font-marker font-bold text-5xl sm:text-7xl m-auto text-center">Juan Lagunas</div>
+                    </div>
+                </div>
             </section>
-            <main>
-                <div className="h-96 bg-zinc-200"></div>
-            </main>
+            <main></main>
         </>
     );
 }
