919af4df779944abcd5e92c415091a47a6c14a84
Juan Lagunas <juanlagunas28@gmail.com>
2023-06-08 20:13:41 -0400

    Change nav

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,7 +1,7 @@
 const Nav = () => {
     return (
         <nav className="fixed w-full h-[90px] font-bold text-center flex items-center">
-            <div className="w-[90%] sm:w-10/12 lg:w-7/12 h-[54px] mx-auto text-white flex justify-between items-center gap-2 bg-[rgba(15,15,15,0.8)] rounded-2xl">
+            <div className="w-[90%] sm:w-10/12 lg:w-7/12 h-[54px] mx-auto text-white flex justify-between items-center gap-2 bg-[rgba(20,20,20,0.8)] rounded-2xl">
                 <div className="pl-4 text-xl">
                     <a href="/" className="sm:hover:scale-110 sm:hover:-translate-y-1 transition ease-in-out duration-200">PBJ</a>
                 </div>
