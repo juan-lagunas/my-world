7c121435460334063cbf2370122a8b310a2bb0e8
Juan Lagunas <juanlagunas28@gmail.com>
2023-06-08 20:20:50 -0400

    Change nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,9 +76,6 @@ export default function World() {
                     </div>
                 </section>
                 
-                
-                
-                
             </main>
         </>
     );
